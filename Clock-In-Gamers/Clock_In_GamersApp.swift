f423304df90a50752f5ed029e3f265ceccc38442
KaiLiu12 <74182067+onetrickollie@users.noreply.github.com>
2024-07-23 13:07:49 -0700

    Initial Commit

---
+++ Clock-In-Gamers/Clock_In_GamersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clock_In_GamersApp.swift
+//  Clock-In-Gamers
+//
+//  Created by KaixiangLiu on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Clock_In_GamersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clock-In-Gamers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clock-In-Gamers
+//
+//  Created by KaixiangLiu on 7/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
