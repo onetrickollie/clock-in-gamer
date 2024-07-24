ec6bb08777ec6713f5b836eca2c5907e60decd15
Victor Mercado <victormercado@outlook.com>
2024-07-24 13:12:21 -0700

    added Persistence file with some example Protocols

---
+++ Clock-In-Gamers/Persistence.swift
@@ -0,0 +1,28 @@
+//
+//  Persistence.swift
+//  Clock-In-Gamers
+//
+//  Created by Victor Mercado on 7/24/24.
+//
+
+import Foundation
+
+protocol BioData {
+    
+}
+
+protocol Calendar {
+    
+}
+
+protocol Games {
+    
+}
+
+protocol Friends {
+    
+}
+
+class FileSystemData : ObservableObject {
+    
+}
